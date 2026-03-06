080a261d887cc287e2f8e650b2c91c48c8e2eae6
bug406 <145317008+GitBug406@users.noreply.github.com>
2026-03-06 19:34:02 +0530

    Fix nested loop indentation in hollowRectangle method
    No Description Just small Indentation

--- 0_JavaProgrammingBasics/Patterns/Patterns.java
+++ 0_JavaProgrammingBasics/Patterns/Patterns.java
@@ -2,7 +2,7 @@ public class Patterns {
     //Hollow Rectangle
     public static void hollowRectangle(int n, int m) {
         for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
+            for(int j=1; j<=m; j++) { //Nested Loop
                 if(i == 1 || j == 1 || i == n || j == m) {
                     System.out.print("*");
                 } else {
